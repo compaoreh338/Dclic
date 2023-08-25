864af0b73a62ac028aabda78d4e317195daf40ca
compaore Hyacinthe <compaoreh338@gmail.com>
2023-08-25 17:30:51 +0000

    laravel configuration

---
+++ database/migrations/2023_08_25_170159_create_cohortes_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cohortes', function (Blueprint $table) {
+            $table->id();
+            $table->string('nomCohorte');
+            $table->unsignedBigInteger('domaineID');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cohortes');
+    }
+};
