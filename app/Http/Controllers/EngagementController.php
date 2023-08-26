2dacc6fd06fffa8c77c7b1617c01b0fed67dc613
Hyacinthe Compaoré <131278191+compaoreh338@users.noreply.github.com>
2023-08-26 09:18:58 +0000

    Feature/my modiv (#3)
    * migration creation
    
    * migration creation

---
+++ app/Http/Controllers/EngagementController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Engagement;
+use App\Http\Requests\StoreEngagementRequest;
+use App\Http\Requests\UpdateEngagementRequest;
+
+class EngagementController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreEngagementRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Engagement $engagement)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Engagement $engagement)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateEngagementRequest $request, Engagement $engagement)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Engagement $engagement)
+    {
+        //
+    }
+}
